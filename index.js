eab91bcb14ea7a54f2d22bf7838a2961b18f47d4
Adeleke Hammed Lekan <lekhad19@gmail.com>
2024-03-12 16:49:51 +0100

    added relationship to category to products relationship

--- index.js
+++ index.js
@@ -347,7 +347,13 @@ const resolvers = {
     }, 
     Category: {
         products: (parent, args, context) => {
-
+            // console.log(parent); 
+            // using destructuring 
+            
+            // const { id } = parent
+            // return products.filter((product) => product.categoryId === id);
+            const categoryId = parent.id;
+            return products.filter((product) => product.categoryId === categoryId);
         } 
     }
 }
