3abeb73b3ff1753055b2673b9612016e28f1fdd4
Adeleke Hammed Lekan <lekhad19@gmail.com>
2024-05-05 15:08:34 +0100

    Invoking context

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -1,6 +1,9 @@
 const { categories } = require("../db");
 exports.Product = {
     category: (parent, args, context) => {
+      // console.log(context); 
+      const { sayHello } = context;
+      sayHello(); 
       const categoryId = parent.categoryId; 
       return categories.find((category) => category.id === categoryId);
     }
